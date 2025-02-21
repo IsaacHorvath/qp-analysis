7156f8c8666533a7954c41a5481f5c08043796c7
Isaac Horvath <isaac.horvath@gmail.com>
2025-02-21 12:22:27 -0500

    move to home page, add 404

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,16 +6,36 @@ use frontend::interface::Interface;
 enum Route {
     #[at("/")]
     Home,
-    #[at("/breakdown")]
-    Interface,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
 }
 
 fn switch(routes: Route) -> Html {
     match routes {
-        Route::Home => html! { <h1>{ "Hello Frontend" }</h1> },
-        Route::Interface => html! { 
+        Route::Home => html! { 
             <Interface />
         },
+        Route::NotFound => html! {
+            <NotFoundPage />
+        }
+    }
+}
+
+#[function_component(NotFoundPage)]
+fn not_found_page() -> Html {
+    let navigator = use_navigator().unwrap();
+
+    let onclick = Callback::from(move |_| navigator.push(&Route::Home));
+    html! {
+        <div>
+            <div style="display: flex; justify-content: center">
+                <h1 style="color: #ffffff">{ "404 not found" }</h1>
+            </div>
+            <div style="display: flex; justify-content: center">
+                <button {onclick} style="background-color: #3f3f3f; border-color: #3f3f3f; border-radius: 10px; color: #ffffff; margin: 5px;">{ "Go Home" }</button>
+            </div>
+        </div>
     }
 }
 
@@ -27,6 +47,8 @@ fn app() -> Html {
         </BrowserRouter>
     }
 }
+
+//#function
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
